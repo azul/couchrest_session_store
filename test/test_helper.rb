ed592863de18697290bb771292c41a7d4918c922
Azul <azul@riseup.net>
2017-04-09 19:18:45 +0200

    cleanup: test_helper require_relative and drop TestClock

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,8 @@
 require 'rubygems'
 gem 'minitest'
 require 'minitest/autorun'
-require File.expand_path(File.dirname(__FILE__) + '/../lib/couchrest_session_store.rb')
-require File.expand_path(File.dirname(__FILE__) + '/couch_tester.rb')
-require File.expand_path(File.dirname(__FILE__) + '/test_clock.rb')
+require_relative '../lib/couchrest_session_store.rb'
+require_relative 'couch_tester.rb'
 
 # Create the session db if it does not already exist.
 CouchRest::Session::Document.create_database!
